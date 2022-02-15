68f83689cca0c74774224b81eab10b5dfeeef771
leticianardi <leticia.tnardi@gmail.com>
2022-02-14 21:35:58 -0800

    first commit and about

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import About from "./components/About/About";
+
+const App = () => {
+  return (
+    <div>
+      <About />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/About/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./about.css";
+
+const about = () => {
+  return (
+    <div className="about-section">
+      <div className="about-left">
+        <div className="a-left-wrapper">
+          <h2 className="about-intro">Hey, it's me,</h2>
+          <h1 className="about-name">Leticia</h1>
+          <div className="a-title">
+            <div className="a-titlle-wrapper">
+              <div className="a-title-item">Web Developer</div>
+              <div className="a-title">job 2</div>
+              <div className="a-title">job 3</div>
+              <div className="a-title">job 4</div>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="about-right">right</div>
+    </div>
+  );
+};
+
+export default about;

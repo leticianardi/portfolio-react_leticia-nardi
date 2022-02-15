983a6838682cbf1ff989c8cee901dba4679ff5d8
leticianardi <leticia.tnardi@gmail.com>
2022-02-15 01:01:50 -0800

    contact component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About/About";
 import AboutMe from "./components/Aboutme/Aboutme";
 import PortfolioList from "./components/PortfolioList/PortfolioList";
+import Contact from "./components/Contact/Contact";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <About />
       <AboutMe />
       <PortfolioList />
+      <Contact />
     </div>
   );
 };
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./contact.css";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">let's chat</h1>
+          <div className="c-info">
+            <div className="c-info-item"></div>
+            <img src="" alt="" className=""></img>
+          </div>
+        </div>
+        <div className="c-right"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;

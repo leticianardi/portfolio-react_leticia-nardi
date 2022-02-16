d2fbe4f3b0a9b0793ca3e10d83fd2ef87072e916
leticianardi <leticia.tnardi@gmail.com>
2022-02-15 23:51:18 -0800

    validation code // not working

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./contact.css";
 import Phone from "../../img/icon-phone.svg";
 import Email from "../../img/icon-email.svg";
 import Github from "../../img/icon-github.svg";
-import { useRef } from "react";
+// import { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
   const formRef = useRef();
+
+  const [setFormState, formState] = useState({
+    user_name: "",
+    user_email: "",
+    message: "",
+  });
+
   const [done, setDone] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const { user_name, user_email, message } = formState;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,9 +36,29 @@ function Contact() {
           setDone(true);
         },
         (error) => {
-          console.log(error.text);
+          if (!errorMessage) {
+            setFormState({ [e.target.name]: e.target.value });
+            console.log("Form", formState);
+          }
         }
       );
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === "user_email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your e-mail is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.user_name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
   };
 
   return (
@@ -61,18 +91,38 @@ function Contact() {
           </p>
 
           <form ref={formRef} onSubmit={handleSubmit}>
-            <input type="text" placeholder="Your name" name="user_name"></input>
+            <input
+              type="text"
+              placeholder="Your name"
+              name="user_name"
+              defaultValue={user_name}
+              onBlur={handleChange}
+            ></input>
             <input
               type="text"
               placeholder="Your e-mail"
               name="user_email"
+              defaultValue={user_email}
+              onBlur={handleChange}
             ></input>
             <input
               type="text"
               placeholder="Subject"
               name="user_subject"
             ></input>
-            <textarea rows="5" placeholder="Message" name="message"></textarea>
+            <textarea
+              rows="5"
+              placeholder="Message"
+              name="message"
+              defaultValue={message}
+            ></textarea>
+
+            {errorMessage && (
+              <div>
+                <p className="error-text">{errorMessage}</p>
+              </div>
+            )}
+
             <button>Submit</button>
             {done && " Thank you for contacting. "}
           </form>
